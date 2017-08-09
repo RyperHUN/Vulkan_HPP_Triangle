b9b7533b5d939d88ad34631f7a9cabfa1d409ae6
Ryperhun <akormanyzsolt@gmail.com>
2017-08-09 12:15:04 +0200

    Added init vulkan project

---
+++ Vulkan Particle/Vulkan Particle.cpp
@@ -0,0 +1,12 @@
+// Vulkan Particle.cpp : Defines the entry point for the console application.
+//
+
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.hpp>
+#include <glm/glm.hpp>
+
+int main()
+{
+    return 0;
+}
+
